cf958176cbdd66384748685be18c033803798814
jcapellanvasquez <jv.capellan06@gmail.com>
2022-02-22 21:36:05 -0400

    clase 9 - ejercicios con el DOM y flexbox

---
+++ clase_9/assets/js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function(event) {
+    console.log("Funciono!")
+    const body = document.getElementsByTagName("body")[0]
+    console.log(body.innerHTML)
+    body.style.backgroundColor = '#EFF1F3'
+})
+
---
+++ clase_9/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('assets'))
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, '/vistas/index.html'));
+})
+
+app.listen(3000)
+console.log('Applicacion corriendo en el puerto 3000')
